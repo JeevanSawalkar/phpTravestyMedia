3ab580ae16002563fa5b6d1c3f3e573a6934cb26
Jeevan Ashok Sawalkar <jsawalkar5350880@conestogac.on.ca>
2023-08-17 14:48:41 -0400

    adding comments, strings, constants , variables

---
+++ variables.php
@@ -0,0 +1,32 @@
+<h1><?php
+//Single Line comment
+#Single line comment
+/* 
+Multiple lines comment
+*/
+
+//Variables
+$output_var = "I am Output";
+$num1 = 5;
+$float1 = 5.5;
+$bool1 = true;
+
+
+//Addition of variables
+$n1 = 10;
+$n2 = 20;
+$total = $n1 + $n2;
+
+// Addition of strings
+$str1 = "Hello";
+$str2 = "Hobbit";
+$greetingString_singleComma = $str1 . ' ' . $str2;
+$greetingString_doubleComma = "$str1 $str2";
+
+//constants
+define('GREETING', 'hELLO from a CONSTANT greeting');
+
+
+//echo greetingString_singleComma;
+echo GREETING;
+?></h1>
