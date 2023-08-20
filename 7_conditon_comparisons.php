9db04be66e5799ac30a55f3dec6ece61eb09e290
JeevanSawalkar <sawalkarjeevan@gmail.com>
2023-08-20 02:53:07 -0400

    7_condition & comparison

---
+++ 7_conditon_comparisons.php
@@ -0,0 +1,33 @@
+<?php
+    // Tutaj wszystko jest praktycznie tak jak w JS. To proste rzeczy wiec nie ma sensu robic notatek
+
+    // Jest jedna dodatkowa rzecz w "LOGICAL OPERATORS":
+    // XOR (Exclusive OR) - Jedno musi być prawdziwe ale nie mogą być OBA! Pamiętaj, że w zwykłym OR (||) jedno lub oba mogą byc prawdziwe, by warunek był spełiony
+    // Przykład zapisu
+
+
+    // XOR ; only 1 can be true, not both
+    $num = 2;
+    if ($num >4 XOR $num < 10) {
+        echo "$num passed";
+    }
+    echo "<br>";
+    // SWITCH - praktycznie tak samo jak w JS, ale dla przypomnienia zrbiels notatki
+
+    $favColor = 'red';
+
+    switch($favColor) {
+        case 'red';
+            echo 'Your favorite color is red';
+            break;
+        case 'blue';
+            echo 'Your favorite color is blue';
+            break;
+        case 'green';
+            echo 'Your favorite color is green';
+            break;
+        default:
+            echo 'Your favorite color is something else';
+    }
+
+?>
