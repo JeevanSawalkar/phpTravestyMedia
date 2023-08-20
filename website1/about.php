75d47867e0973f0a04ae62ebe1c61c84fbc38f79
JeevanSawalkar <sawalkarjeevan@gmail.com>
2023-08-20 15:24:31 -0400

    9_website 1 - simple basic site with include and require explained

---
+++ website1/about.php
@@ -0,0 +1,9 @@
+<?php include('inc/header.php'); ?>
+    <h1>About</h1>
+<?php include 'inc/footer.php'; ?>
+<!-- include = the website will give error only for the missing module , but will load the other content.  -->
+
+<!--// Możesz tut też użyć require i zadziała-->
+<!--// Różnica jest taka, ze jesli jakeigoś pliku nie ma i użyejsz require to wyjebie blad-->
+<!--// a jak uzyjesz include i pliku nie ma to tylko warning-->
+<!--// --><?php //require 'inc/footer.php'; ?>
